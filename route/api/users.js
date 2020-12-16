b37ebc58bf1cfc4a7f2c152ef61dff164bf9e06c
zombie-org <bzombie589@gmail.com>
2020-12-17 01:14:38 +0530

    Added User registration

--- route/api/users.js
+++ route/api/users.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const { check, validationResult } = require("express-validator/check");
 const router = express.Router();
+
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
+
+//create User model for DB
+const User = require("../../models/User");
+
 // @route   POST api/users
 // @desc    TO verify and handle user input req
 // @access  Public
@@ -13,12 +20,51 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User Route");
+
+    const { name, email, password } = req.body;
+    //user Registeration
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      //get user Gravatar
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //Encrypt Password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      //return jsonwebtoken
+
+      res.send("User Register");
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send("Server Error");
+    }
   }
 );
 
